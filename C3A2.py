4f07ccb3da5e86aaf1565f78c642483afcd910e8
CHIRAG JAT <94890422+chirag787@users.noreply.github.com>
2021-11-24 22:39:46 -0800

    Add files via upload

---
+++ C3A2.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 24 22:32:09 2021
+
+@author: lenovo
+"""
+
+import pygame, sys
+
+pygame.init()
+clock=pygame.time.Clock()
+
+screen = pygame.display.set_mode((400,600))
+
+player=pygame.Rect(200,500,30,30)
+playerSpeed=20
+enemy=pygame.Rect(70,50,40,40)
+enemyspeed= -2;
+
+while True:    
+    screen.fill((0,0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.x -=playerSpeed
+            if event.key ==pygame.K_RIGHT:
+                player.x +=playerSpeed
+            if event.key == pygame.K_SPACE:
+               print("bullet fired")
+         
+    enemy.x= enemy.x + enemyspeed
+    
+    if enemy.x == 0:
+        enemyspeed=enemyspeed * -1
+        enemy.y= enemy.y + 20
+    
+    if enemy.x == 380:
+        enemyspeed=enemyspeed * -1
+        enemy.y= enemy.y + 20
+    
+           
+    pygame.draw.rect(screen,(123,200,100),enemy)
+    pygame.draw.rect(screen,(23,100,100),player)
+    
+    
+    pygame.display.update()
+    clock.tick(30)
